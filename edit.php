9aa79f5f8a50e905944192c6359ebae0d9eb3bbb
Joooooooooooooon <jjoon1016@gmail.com>
2022-09-15 11:57:29 +0900

    更新処理作成

--- edit.php
+++ edit.php
@@ -1,19 +1,42 @@
+<?php
+require_once __DIR__ . '/functions.php';
+
+/* タスク更新処理
+---------------------------------------------*/
+// 初期化
+$title = '';
+$errors = [];
+
+// index.php から渡された id を受け取る
+$id = filter_input(INPUT_GET, 'id');
+
+// 受け取った id のレコードを取得
+$task = find_task_by_id($id);
+
+// リクエストメソッドの判定
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // フォームに入力されたデータを受け取る
+    $title = filter_input(INPUT_POST, 'title');
+
+    // バリデーション
+    $errors = update_validate($title, $task);
+
+    // エラーチェック
+    if (empty($errors)) {
+        // タスク更新処理の実行
+        update_task($id,$title);
+        
+        // index.php にリダイレクト
+        header('Location: index.php');
+        exit;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Tasks</title>
-    
-    <!-- CSS -->
-    <link rel="stylesheet" href="https://unpkg.com/ress@3.0.0/dist/ress.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&family=Zen+Maru+Gothic:wght@300;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
+
+<?php include_once __DIR__ . '/_head.html' ?>
+
 <body>
     <div class="wrapper">
         <header class="header-task">
@@ -22,11 +45,10 @@
         <h2 class="update-task-title">タスクの更新</h2>
         <div class="update-task task-form-group">
             <!-- エラーが発生した場合、エラーメッセージを出力 -->
-            <ul class="errors">
-                <li>タスク名を入力してください</li>
-            </ul>
+            <?php require_once __DIR__ . '/_errors.php' ?>
+            
             <form action="" method="post">
-                <input type="text" name="title" placeholder="タスクを入力してください">
+                <input type="text" name="title" placeholder="タスクを入力してください" value="<?= h($task['title']) ?>">
                 <div class="update-btn-group">
                     <button type="submit" class="big-btn update-btn">
                         <span>Update</span>
@@ -41,4 +63,5 @@
         </div>
     </div>
 </body>
-</html> 
+
+</html>
